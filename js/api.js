e076a7134431900904d8409a0f2e81b56155ae3d
madhurdigitals <madhurdigitals01@gmail.com>
2026-03-30 13:52:47 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,35 +1,23 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbytZo8tG54g4sAlcKSmL7VPEQ_I1uNILLcOB9tsUjRqHGNGqKxjv4w82-rcNU8W-H_xTg/exec";
 
-// ✅ ADD STUDENT
 async function addStudent(data) {
   const res = await fetch(API_URL, {
     method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json"
-    },
     body: JSON.stringify({
       action: "addStudent",
       ...data
     })
   });
-
-  return await res.json();
+  return res.json();
 }
 
-// ✅ GET STUDENTS
 async function getStudents(school) {
   const res = await fetch(API_URL, {
     method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json"
-    },
     body: JSON.stringify({
       action: "getStudents",
       school: school
     })
   });
-
-  return await res.json();
+  return res.json();
 }
