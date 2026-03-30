69f39d5c8f7d61b0f2104a7839941b7171e2da88
madhurdigitals <madhurdigitals01@gmail.com>
2026-03-30 13:45:48 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "PASTE_NEW_DEPLOYMENT_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbytZo8tG54g4sAlcKSmL7VPEQ_I1uNILLcOB9tsUjRqHGNGqKxjv4w82-rcNU8W-H_xTg/exec";
 
 // ✅ ADD STUDENT
 async function addStudent(data) {
