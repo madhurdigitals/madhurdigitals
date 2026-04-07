0cb7122b089f4f4762d3e810f9da73390fe212d6
Abhishek Maurya <madhurdigitals01@gmail.com>
2026-04-07 15:16:46 +0530

    Initial commit

--- js/api.js
+++ js/api.js
@@ -2,7 +2,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbytZo8tG54g4sAlcKSmL7VPEQ_I1uNILLcOB9tsUjRqHGNGqKxjv4w82-rcNU8W-H_xTg/exec";
 let schoolCache = null;
 let schoolsData = [];
-
 /**
  * ✅ ADD STUDENT (using GET to avoid CORS)
  */
